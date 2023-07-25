bd84440d944b8df40e61ee32c662f46d780b1cc8
Egehan Yıldız <55427777+EgehanYildiz@users.noreply.github.com>
2023-07-25 14:17:53 +0000

    new change

--- Components/MySearchBar.js
+++ Components/MySearchBar.js
@@ -6,8 +6,7 @@ const MySearchBar = ({labelName, text, setText, addTodos}) => {
   return (
     <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
       <InputText value={text} onChange={(e) => setText(e.target.value)} style={{ flex: '1', borderRadius: '7px', marginBottom: '10px', height: '40px'}} />
-      <Button label={labelName} onClick={addTodos} 
-      className='text-white ml-1 border-round-lg mb-3 border-solid border-1 border-white bg-bluegray-900'/>
+      <Button label={labelName} onClick={addTodos} className='text-white ml-1 border-round-lg mb-3 border-solid border-1 border-white bg-bluegray-900'/>
     </div>
   )
 }
