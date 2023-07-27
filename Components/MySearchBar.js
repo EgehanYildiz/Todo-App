6cc9bd26db08bf2cec1d6a24f6446610b1c199e8
Egehan <egehanyldz7@gmail.com>
2023-07-27 15:56:13 +0300

    tasks deleted and h-3rem turned into h-auto for full responsibility: there was a problem with some real phones the styling is corrupted hopefully this fix it.

--- Components/MySearchBar.js
+++ Components/MySearchBar.js
@@ -4,11 +4,11 @@ import { Button } from 'primereact/button';
 
 const MySearchBar = ({labelName, text, setText, addTodos}) => {
   return (
-    <div className='flex justify-content-between align-items-center'>
+    <div className='flex rounded justify-content-between align-items-center'>
       <InputText  value={text} onChange={(e) => setText(e.target.value)} 
-      className='surface-0 flex-1 border-round-lg mb-3 h-3rem'/>
+      className='surface-0 flex-1 border-round-lg mb-3 h-auto'/>
       <Button label={labelName} onClick={addTodos} 
-      className='text-white ml-1 justify-content-center align-items-center border-round-lg mb-3 border-solid border-1 border-100 surface-0 h-3rem '/>
+      className='text-white ml-1 justify-content-center align-items-center border-round-lg mb-3 border-solid border-1 border-100 surface-0 h-auto '/>
     </div>
   )
 }
